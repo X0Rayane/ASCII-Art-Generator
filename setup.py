ec063a77a809ad74fd8c86ac3711223a9f137a5b
Rayane GUÉROU <rayane.guerou@edu.devinci.fr>
2024-01-29 14:31:22 +0100

    creating the setup.py for the Licence

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{ASCII_Art_Generator}',
+ version='1.0',
+ author='{X0Rayane}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
