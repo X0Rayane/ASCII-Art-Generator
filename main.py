a864e7fa34c7f065c7dcef94ccbe20c28ebe3422
Killian Lafaye <traf.scay@gmail.com>
2024-01-30 18:14:30 +0100

    added colors option

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyfiglet
+from colorama import Fore, Style
 
 class FigletASCIIArtGenerator:
     def __init__(self, font='standard'):
@@ -21,6 +22,7 @@ def main():
     figlet_ascii_art_generator = FigletASCIIArtGenerator(default_font)
 
     while True:
+        print(Style.RESET_ALL)
         user_input = input("Enter your text or 'change_font' to set a new font (type 'exit' to quit): ")
         if user_input.lower() == 'exit':
             break
@@ -28,7 +30,28 @@ def main():
             new_font = input("Enter the font name: ")
             figlet_ascii_art_generator.change_font(new_font)
         else:
+            color = input("Choose the color of your ascii text :\n BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE")
+            color = color.upper()
+            if(color == 'BLACK'):
+                print(Fore.BLACK)
+            elif(color=='RED'):
+                print(Fore.RED)
+            elif(color == "GREEN"):
+                print(Fore.GREEN)
+            elif(color == "YELLOW"):
+                print(Fore.YELLOW)
+            elif(color == "BLUE"):
+                print(Fore.BLUE)
+            elif(color == "MAGENTA"):
+                print(Fore.MAGENTA)
+            elif(color== "CYAN"):
+                print(Fore.CYAN)
+            elif(color == "WHITE"):
+                print(Fore.WHITE)
+            else:
+                continue
             figlet_ascii_art_generator.generate_ascii_art(user_input)
 
 if __name__ == "__main__":
     main()
+
